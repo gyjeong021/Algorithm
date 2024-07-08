fb93ba550f4d87fd47cfc5d1beb8f097830c95fc
jiyoung <gyjeong021@gmail.com>
2024-07-08 22:31:23 +0900

    240708_문제풀이

---
+++ baekjoon/스택/sliver/3986_좋은단어.py
@@ -0,0 +1,23 @@
+# 문제 설명 : A와 B 각각이 짝을 이루어 선이 교차하지 않도록 매칭되는 개수 출력
+# 핵심 개념 : 스택 (후입선출)
+
+n = int(input())
+cnt = 0
+
+for _ in range(n) :
+    stack = []
+    sentence = input()
+    for i in sentence :
+        if len(stack) == 0 :
+            stack.append(i)
+        elif i == stack[-1] :
+            stack.pop(-1)
+        else :
+            stack.append(i)
+
+    if len(stack) == 0 :
+        cnt += 1
+
+print(cnt)
+
+        
---
+++ baekjoon/스택/sliver/9012_괄호.py
@@ -0,0 +1,22 @@
+# 문제 설명 : '('와 ')'의 개수가 잘 매칭되어 구성되어있는지 확인
+# 핵심 개념 : 스택 (후입선출)
+
+n = int(input())
+
+for _ in range(n) :
+    stack = []
+    sentence = input()
+    for i in sentence :
+        if i == "(" :
+            stack.append(i)
+        elif i == ")" :
+            if len(stack) != 0 :
+                stack.pop()
+            else :
+                print("NO")
+                break
+    else : # for 루프가 break 없이 끝까지 실행된 경우
+        if len(stack) == 0 :
+            print("YES")
+        else :
+            print("NO")
