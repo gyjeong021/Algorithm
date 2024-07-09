08e569d831fa95d3673ac23018d1ca1cf68ef5c1
jiyoung <gyjeong021@gmail.com>
2024-07-09 10:20:15 +0900

    240709_문제풀이

---
+++ baekjoon/큐/sliver/18258_큐2.py
@@ -0,0 +1,36 @@
+# 문제 설명 : 큐 함수 명령을 처리하는 프로그램 작성
+# 핵심 개념 : 큐 Queue - FirstinFirstOut(FIFO, 선입선출)
+
+import sys
+from collections import deque
+
+n = int(input())
+que = deque()
+
+for _ in range(n) :
+    x = sys.stdin.readline().split()
+    if x[0] == "push":
+        que.append(x[-1])
+    elif x[0] == "pop" :
+        if len(que) == 0 :
+            print(-1)
+        else :
+            print(que.popleft())
+    elif x[0] == "size" :
+        print(len(que))
+    elif x[0] == "empty" :
+        if len(que) == 0 :
+            print(1)
+        else :
+            print(0)
+    elif x[0] == "front" :
+        if len(que) == 0 :
+            print(-1)
+        else :
+            print(que[0])
+    elif x[0] == "back" :
+        if len(que) == 0 :
+            print(-1)
+        else :
+            print(que[-1])
+ 
