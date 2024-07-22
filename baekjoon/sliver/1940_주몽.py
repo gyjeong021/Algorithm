e8eafc92eb9c99d37a91776fd9d13935857b29ec
jiyoung <gyjeong021@gmail.com>
2024-07-22 23:36:12 +0900

    240722_문제풀이

---
+++ baekjoon/sliver/1940_주몽.py
@@ -0,0 +1,29 @@
+# 알고리즘 분류 : 투포인터
+# 문제 설명 : 재료 N으로 몇개의 갑옷(M)을 만들 수 있을지 구하기
+# 핵심 개념 :
+
+import sys
+
+n = int(input())
+m = int(input())
+n_num = list(map(int, sys.stdin.readline().split()))
+
+n_num.sort()
+left = 0
+right = len(n_num) - 1
+count = 0
+
+while left < right :
+    sum = n_num[left] + n_num[right]
+    if sum < m :
+        left += 1
+    elif sum > m :
+        right -= 1
+    else :
+        count += 1
+        left += 1
+        right -= 1
+
+print(count)
+    
+
